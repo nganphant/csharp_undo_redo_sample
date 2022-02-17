e57786640cca8919e3b7051473f153783b3f0fe1
phan thành ngân <phan.thanh.ngan@use-inc.co.jp>
2022-02-17 18:53:16 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace UndoCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(35, 678);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(231, 678);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1077, 648);
+            this.label1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(35, 751);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 52);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Undo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Location = new System.Drawing.Point(231, 751);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 52);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Redo";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 839);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UndoCSharp
+{
+    public partial class Form1 : Form
+    {
+        class ACTION
+        {
+            public Action undo;
+            public Action redo;
+        }
+
+        Stack<ACTION> undo_stack = new Stack<ACTION>();
+        Stack<ACTION> redo_stack = new Stack<ACTION>();
+        StringBuilder s = new StringBuilder(1000);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void Undo()
+        {
+            if (undo_stack.Count == 0)
+            {
+                return;
+            }
+            ACTION action = undo_stack.Pop();
+            redo_stack.Push(action);// add action nay vo redo -> magic
+
+            action.undo.Invoke();
+        }
+        void Redo()
+        {
+            if (redo_stack.Count == 0)
+            {
+                return;
+            }
+            ACTION action = redo_stack.Pop();
+            undo_stack.Push(action);// add action nay vo undo -> magic
+
+            action.redo.Invoke();
+        }
+
+        private void add_text(string str, int at)
+        {
+            ACTION action = new ACTION()
+            {
+                undo = () =>
+                {
+                    s.Remove(at, str.Length);
+                    label1.Text = s.ToString();
+                },
+                redo = () =>
+                {
+                    s.Insert(at, str);
+                    label1.Text = s.ToString();
+                }
+            };
+
+            undo_stack.Push(action);
+            action.redo();
+        }
+        private void remove_text(int idx, string str)
+        {
+            ACTION action = new ACTION()
+            {
+                undo = () =>
+                {
+                    s.Insert(idx, str);
+                    label1.Text = s.ToString();
+                },
+                redo = () =>
+                {
+                    s.Remove(idx, str.Length);
+                    label1.Text = s.ToString();
+                }
+            };
+
+            undo_stack.Push(action);
+            action.redo();
+        }
+
+        string[] texts = { "Chuyện hôm qua ", "như nước chảy", " về đông",
+" Mãi", " xa ta không", " sao giữ được",
+" Hôm nay ", "lại có bao chuyện", " ưu phiền ", "làm rối cả lòng ta",
+" Rút dao chém", " xuống nước, ", "nước càng ", "chảy mạnh",
+" Nâng chén tiêu sầu,", " càng sầu thêm" };
+        int iiiii = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            add_text(texts[iiiii], s.Length);
+            redo_stack.Clear();// always clear redo when user do some action
+
+            iiiii++;
+            if (iiiii >= texts.Length) iiiii = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // just find a good string to remove, nothing special
+            string aaa = s.ToString().Trim();
+            int idx_of_space = aaa.IndexOf(' ');
+            if (idx_of_space == -1)
+            {
+                return;
+            }
+            int idx_of_space_next = aaa.IndexOf(' ', idx_of_space + 1);
+            if (idx_of_space_next == -1)
+            {
+                return;
+            }
+
+            remove_text(idx_of_space, aaa.Substring(idx_of_space, idx_of_space_next - idx_of_space));
+
+            redo_stack.Clear();// always clear redo when user do some action
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Undo();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Redo();
+        }
+    }
+}
